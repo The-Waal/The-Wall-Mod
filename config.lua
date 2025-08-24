8508733d554c49c414bbd5af57a724b23c30d4f4
The Waal <bestguyever777.real@gmail.com>
2025-08-24 16:09:12 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,4 +6,15 @@ return {
 	["Base"] = 100,
 	["Blind_Scaling"] = "None",
 	["Blind_Scaling_ID"] = 1,
+	["Blind_Custom"] = { --1=small 2=big 3=boss 4=showdown 5=removed
+		["Small"] = 1,
+		["Big"] = 2,
+		["Boss"] = 3,--cant be removed
+		["Small_SD"] = false,
+		["Big_SD"] = false,
+		["Boss_SD"] = true,
+		["ShowdownToggle"] = true,
+		["Showdown"] = 4,--only removed if possible
+	},
+	["Other_Things"] = true
 }
